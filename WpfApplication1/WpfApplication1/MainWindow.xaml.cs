2a7fe35b09fb90cdc468a380cd11a2b7ea4f7521
ivan.sokolov <sjvmaster@gmail.com>
2012-07-08 22:13:27 +0300

    Hello World app

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var person = new MyClass() {LastName = "ololo", FirstName = "trololo"};
+
+            this.DataContext = person;
+        }
+
+        private void PushMeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(FirstName.Text))
+            {
+                GreetingText.Text =
+                    string.Format("Hello, {0}!", FirstName.Text);
+            }
+        }
+
+    }
+}
---
+++ WpfApplication1/WpfApplication1/MyClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WpfApplication1
+{
+    class MyClass
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+    }
+}
